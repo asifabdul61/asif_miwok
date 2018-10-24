764810a6d7abff50d6a43ec0ea2bfdb679079160
Abdul <asifabdul61@gmail.com>
2018-10-24 23:11:41 +0530

    4 activities added

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+    public void openNumbers(View view){
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+
     }
 
 
