5d3fde04acee1183fcf2bb12338cb1704b846ee8
Abdul <asifabdul61@gmail.com>
2018-10-25 00:04:02 +0530

    updated to display numbers

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,12 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        int i = 0;
+        //ArrayList<int> number = new ArrayList<int>();
+        LinearLayout number_layout = (LinearLayout) findViewById(R.id.activity_numbers);
+        for(i = 0;i<40;i++){
+            TextView num = new TextView(this);
+            num.setText("" + (i+1));
+            number_layout.addView(num);
+        }
     }
 }
